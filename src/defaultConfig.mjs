ad63f674cbe0f72c8acba4ea0af3b44c7ff01a7e
Jascha Ehrenreich <jascha@artificialmuseum.com>
2026-02-06 05:36:43 +0100

    defaultConfig: htmlWhitespaceSensitivity is css, add singleAttributePerLine to false

--- src/defaultConfig.mjs
+++ src/defaultConfig.mjs
@@ -8,5 +8,6 @@ export const config = {
   bracketSpacing: true,
   arrowParens: 'avoid',
   plugins: [],
-  htmlWhitespaceSensitivity: 'strict',
+  htmlWhitespaceSensitivity: 'css',
+  singleAttributePerLine: false
 }
